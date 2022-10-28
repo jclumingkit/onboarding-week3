0d6afa0a5b7d5ef1d59c4802737bca057cc843c3
Juan Carlos Lumingkit <juancarloslumingkit@gmail.com>
2022-10-28 16:17:40 +0800

    added mantine form

---
+++ components/SigninPage.tsx
@@ -0,0 +1,40 @@
+import { TextInput, PasswordInput, Button, Group, Box } from "@mantine/core";
+import { useForm } from "@mantine/form";
+
+export default function SigninPage() {
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      password: (value) =>
+        value.length >= 6 ? null : "Password must be atleast 6 characters",
+    },
+  });
+
+  return (
+    <Box sx={{ maxWidth: 300 }} mx="auto">
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <TextInput
+          withAsterisk
+          label="Email"
+          placeholder="your@email.com"
+          {...form.getInputProps("email")}
+        />
+
+        <PasswordInput
+          withAsterisk
+          label="Password"
+          {...form.getInputProps("password")}
+        />
+
+        <Group position="right" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
+    </Box>
+  );
+}
